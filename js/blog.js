a19f139d078edc0ee1c71aa8412794d61c2888f7
antony004 <rojasj572@gmail.com>
2021-02-16 22:43:19 -0600

    1st blog
    test run

--- js/blog.js
+++ js/blog.js
@@ -2,13 +2,41 @@ let myBlogs =
     [
         {
             id: 0,
-            title: "My First Blog",
-            date: "Jan 21 2020",
-            text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et magna vitae metus fermentum interdum. Nulla sapien turpis, ultricies sit amet tristique id, congue eu mauris. Donec fringilla leo a augue congue, non interdum lorem bibendum. Vivamus lobortis nulla mi, in auctor enim rutrum quis. Nunc luctus orci et mi cursus congue. Pellentesque ultrices mi vitae maximus ullamcorper. Aliquam neque lectus, aliquet sit amet scelerisque nec, facilisis in metus. Sed nec diam sed urna rutrum lobortis. `,
-            coverImage: "image-19.jpg",
+            title: "Surfing the Great Lakes?!",
+            date: "Feb 17 2021",
+            text: `It’s possible! All my life I’ve been led to believe that it could only be done out in the ocean. Buoy was I wrong. There is one drawback to it though: the better waves are made during the colder months. Not only does one have to brave the freezing gusts but also the ice-cold water that wraps your face. In a way it’s a test of your determination and perseverance, and some will say, of your sanity ;). For some lake surfers it’s a way to catch the thrill; to make memories with other fellow surfers while filling the adrenaline meter. For others, it’s a way to connect with nature and the spiritual self. Whatever the reason is, it’s definitely an experience to try at least once in your lifetime.
+
+Locations and conditions
+
+*Although all of the Great Lakes are surfable, this will be focused on Lake Michigan. Could possibly do an update once I venture out to the other lakes.
+
+The short: look for the jetties.
+The long: look for the jetties. They make it easier and less tiring to paddle out. The current along them will pull you in further to the deep and all there is to do is lay on the board and enjoy the ride. A couple popular beaches with a few of them also happen to have surf shops nearby, <a href="https://www.lakeeffectsurfshop.com> Lake Effect shop in Milwaukee, WI </a> and {link} Third Coast shop in St. Joseph, MI. They will be glad to guide anyone in the process of getting started. If you want to try the sport, they have the equipment to rent for a day. If you happen to fall in love with it, then it’s highly recommended to invest in your own gear. That way you can explore different parts of the lake.
+
+Once you have your own gear the next thing is to know which spots work best for certain wind directions. Have in mind that lake waves are created by the winds, unlike the ocean where they’re usually caused by storm swells. {bold text} <b> Typically, if you’re on the West side of the lake, North and Northeast winds will allow it to get clean, long waves. Whereas North, Northwest winds will do the trick for the East shore </b> {/bold text}. To see wave and wind reports check out {link} this page. Anywhere from 4 ft to 9 ft waves are ideal to go out in. Anything above that and you’re running the risk of getting injured or even drowning. Oh! Speaking of that - try to bring someone with you when possible. If anything were to go wrong they can be there to assist in any way. Remember that lifeguards are off duty in the winter and the majority of the public is inside their cozy homes. There is more technicality to the winds, but for the most part that rule of thumb will get you out catching waves. Not going to say choppy or washing machine waves are not fun to be out in either. You would just have to be in tip-top condition to be fast enough to get back on the board repeatedly. Not to mention the endurance needed for such task.
+
+Gear
+
+The most important part for the harsh winter conditions will have to be the wetsuit. As Sir Mix-a-Lot once suggested, [you] want them real thick. {bold text} A {vissla link} 6/5mm hooded suit, paired with {vissla link} 7 or 8mm gloves and booties will keep you warm for long periods of time and could be used from Autumn to Spring {/bold text}. The vissla hooded suit may be the best deal for your money. It competes well against the more expensive options out there like Xcel. Once the weather gets warm, one can opt for the 2 or 3mm shorty wetsuit. From my understanding, there aren’t as many surf days in the summer so one could possibly do without a suit. I say this with uncertainty because I have yet to try summer surfing out here, although I hear it’s limited to just a few locations due the fact that one can’t surf the public beaches.
+
+{bold text} When it comes to the surfboard, it is recommended to start long and make your way shorter. {/bold text} The drawback to this approach is that the longer the board is, the more expensive it tends to be. For beginners, one could start with a {link} foam (which costs fraction of the price of a hardtop) longboard (8’ or longer) with a 2 + 1 fin setup (for better stability (more on fins {link} here)). You won’t feel as guilty when it gets damaged and it’ll be less painful when colliding with it. Trust me, there will be a lot of that out there. Longboards also make it easier to catch waves of all sizes, including knee high ones, which allows you for more practice time thus will make you a better surfer faster. It’s not unheard of to start with a short board though. If you’re one that considers yourself athletic, there is a higher chance that the short board won’t be as difficult to learn on. Be advised though, surfing does require a lot of patience in order to fully understand the rhythm of the waves and to be able to determine the pop-up time, so if patience is not one of your virtues, start with the longboard. Most boards include the fins and sometimes the leash as well.  The final piece of the puzzle will have to be the wax used for traction. There are a lot of brands out there. Get any one that reads extra cold and you’ll be set.
+
+Common courtesy
+
+There is some surf etiquette to follow when being out in the water. One of them being when paddling out, paddle away from other surfers to avoid injuries. Also there should be one person per wave; the closest to the peak has the right away. Do not drop in while another surfer is riding it and do not throw your board in danger of others. Being courteous will definitely score you points with the surfer community and could even lead to a camaraderie where they tell you of the best local spots that not many people know of. Speaking of those secluded spots, if you do happen to come across them, make sure to not publicize them on social media to avoid them getting over-crowded. One of the fun things about lake surfing is that there always will be a search for the good spots. It’s just an adventure to go out to find it as it is to ride those waves. 
+
+…
+
+Good luck on your surfing journey! Have fun out there and be safe. Oh! Almost forgot the most important thing of all. Watch out for the fresh-water sharks (Jaws theme intensifies)!
+
+
+`,
+            coverImage: "image-000.jpg",
             images: [
-                { filename: "image-19.jpg", caption: "19 image" },
-                { filename: "image-20.jpg", caption: "20 image" }
+                { filename: "image-001.jpg" },
+                { filename: "image-002.jpg" },
+                { filename: "image-003.jpg" }
+                
             ]
         },
 
