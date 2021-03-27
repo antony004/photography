8b985d6eb2c989cc4bb04b4a69b8f60e9a9a472b
antony004 <rojasj572@gmail.com>
2021-03-27 11:26:04 -0500

    test commit
    test run

--- js/blog.js
+++ js/blog.js
@@ -2,22 +2,37 @@ let myBlogs =
     [
         {
             id: 0,
+            title: "Ocean dreams in fresh water lakes",
+            date: "Mar 27, 2021",
+            text: `When thinking about surfing, one of the first few places that come to mind are California and Hawaii, with their barrel, salted waves and multicolor sunsets. Or perhaps Costa Rica, with its tropical waters and beaches filled with coconuts and palm trees; monkeys swinging from tree to tree and parakeets flying over the breaking waves. Typically, surfing is thought to be an ocean activity done during the warmer seasons. What not too many people are aware of is that one doesn’t have to fly hundreds of miles or wait until summer to ride the waves. It can be done right in our backyards, in the fresh water lakes. Being that the Great Lakes could be surfed on during the colder months, one has to brave the freezing gusts and the ice-cold water that engulfs one’s face. In a way though it’s a test of one’s determination and perseverance, and although it’s seems like an absurd thing to do, the joy achieved from riding the exhilarating waves definitely trumps the few thoughts of being cold.  For some lake surfers it’s a way to catch the thrill and to make memories with other fellow surfers while filling the adrenaline meter. For others, it’s a way to connect with nature and the spiritual self. Whatever the reason is, it’s definitely an experience to try at least once in a lifetime, as long as it’s done in the proper equipment.
+ 
 
+            Gear
 
+            The most important part for the harsh winter conditions will have to be the wetsuit. {bold text} A <a href="https://www.vissla.com/shop/shop/seven-seas-6-5-hood-chest-zip-blk/">6/5mm hooded suit</a>, paired with 7 or 8mm <a href="https://www.lakeeffectsurfshop.com/product-page/vissla-7mm-seven-seas-mitten">gloves</a> and <a href="https://www.thirdcoastsurfshop.com/vissla-seven-seas-round-toe-bootie-7mm-black.html">booties</a> will keep one warmer for longer periods of time and can be used from Autumn to Spring {\bold text}. The Vissla hooded suit linked above may be the best deal for your money as it competes well when compared to more expensive options, such as Xcel and Patagonia. Once the weather gets warmer, one can opt for the 2 or 3mm Springsuit (also known as a “shorty wetsuit”) and skip the gloves and booties. Most online shops have a sizing chart specific to their suits so make sure to pay attention to each provider’s individual information. Measuring correctly will avoid the hassle of returning or exchanging the suit, and will get you out catching waves quicker.
 
 <<<<<<< Updated upstream
 The short: look for the jetties.
 The long: look for the jetties. They make it easier and less tiring to paddle out. The current along them will pull you in further to the deep and all there is to do is lay on the board and enjoy the ride. A couple popular beaches with a few of them also happen to have surf shops nearby, <a href="https://www.lakeeffectsurfshop.com"> Lake Effect shop in Milwaukee, WI </a> and {link} Third Coast shop in St. Joseph, MI. They will be glad to guide anyone in the process of getting started. If you want to try the sport, they have the equipment to rent for a day. If you happen to fall in love with it, then it’s highly recommended to invest in your own gear. That way you can explore different parts of the lake.
 =======
+            {bold text} When selecting surfboards, it is recommended to start longer in length and as one’s skills improve, transition to a short board {\bold text}. A great board for beginners would be a {link} foam longboard with a three-fin setup (more on fins {link: https://swellsurfcamp.com/surfboard-fins/} here), which can be found at a fraction of the price of a hardtop board. When starting with a foam board, if collisions do occur, it will be less painful for you as well as your wallet. Longboards make it easier to catch waves of all sizes thus allowing for more practice time. While longboards have many advantages, deciding to use a shortboard can also provide great learning opportunities. If you consider yourself to be athletic, there is a higher chance that the shortboard won’t be as difficult to learn on. Despite physical ability, be advised that surfing does require a lot of patience in order to fully understand the rhythm of the waves and to be able to determine the “pop-up”, or the movement done to quickly stand up on the board. If patience is not one of your strengths, it is advised to start with a longboard. Regardless of the length, most board purchases will include the fins and potentially the leash. The final touch in completing the board setup will be the wax used to enhance traction. While there are a lot of brands out there, selecting any one that reads “extra cold” will be the best option for the great lakes. An optional item is earplugs, as surfing for extended periods of time in cold water can cause surfer’s ear. This is a condition where extra bone forms as lumps inside the ear canal, which is believed to be the body’s defense mechanism to protect the eardrum from extreme water temperatures and strong winds. One is able to get these and other items on the list online or at the local surf shop.
 >>>>>>> Stashed changes
 
+            Lake Michigan has some popular beaches with surf shops nearby such as {link} The Lake Effect shop in Milwaukee, WI, offering everything from window decals to hardtop boards. Jake, the owner, is an eccentric person that has surfed most of his life and is eager to discuss his great knowledge of the sport. The {link} Third Coast shop in St. Joseph, MI has all types of gear for surfing, and even branches out to snowboarding and sand boarding. The manager, Dee, has a passion for surfing with a laidback style that puts even the most novice surfer at ease when discussing gear selection or surfing technique. Both shops have the complete surfing equipment necessary to rent for a day if purchasing the gear isn’t desired. If you do fall in love with surfing, it’s highly recommended to invest in your own gear down the road. It enables one to explore different parts of the lake with flexibility according to one’s schedule. 
 
+            Conditons
 
+            Once gear has been established, the next thing to determine is which spot works best for certain wind directions. Keeping in mind that lake waves are created by wind, differing from ocean waves, which are usually created by storm swells. {bold text} Typically, if you’re on the west side of the lake, north and northeast winds will create clean, long waves. Where north and northwest winds will produce the same quality waves for the east shore {\bold text}. To see current wave and wind reports check out {links, starting with 3rd coast this time) this page or this page. The ideal height for beginners to catch waves is a range from 3 feet to 9 feet; waves above 9 feet can create a more risky situation, which could cause injury or drowning. Man made structures can calm waters too and affect the waves, such as jetties and breakwaters. Jetties oftentimes create currents alongside them that will pull surfers in further to the deep, resulting in less paddling and reserving the surfer’s energy.  Choosing the correct side of rock structures or the appropriate wave height to surf on will make the experience more enjoyable.
 
+            Surfing is a sport with many variables, and wave conditions are never consistent, making each year different from the previous one. For the most part, surfing in the Great Lakes begins around October and goes until mid January.  Second half of January and all of February tend to be the coldest times when most of the shores will be frozen. The waters will be slushy and it will be rare to find an open area free of ice; it’s not until mid March that one can get back into the water.  Typically, there aren’t as many freshwater surf days in the summer as there are in the winter, and the waters are also calmer during the summer. Oftentimes during the summer, surfing is limited due to a lack of available locations since the public beaches are open. It is during the summer that one can go explore other beaches of the world and utilize the valuable experience and skills learned on the lakes. Taking advantage of various seasons to enhance one’s surfing skills is ideal, but one must always remember to follow the surfer’s code of conduct, regardless of the season or location.
 
+            Surfing Etiquette
 
+            One’s conduct on the lake is the same throughout the globe. These codes of conduct are common practice within the surfing community; they unite the wild spirits of humanity and create a rare bond that keeps the sport unique and pleasurable. By following the codes of conduct, one can create connections, allowing for knowledge to flow between surfers, whether it is regarding secluded surfing spots, proper technique, or even food joints to go to after each session. The most important code is to not reveal or geotag the exact location of the good surfing spots on social media. If one does post the coveted location, that may attract others and the spot might get overcrowded, leading to competition for waves. The next code of conduct is that there should be one person per wave, with the exception of party waves.  The closest surfer to the peak of the wave has the right of way and trying to join a wave that another surfer is riding is called “dropping in” or “snaking” another surfer.  Per common surf law, this is forbidden.  Everyone is trying to keep the surfing experience as safe as possible and that includes being mindful of your own behaviors. Another important code is that when paddling out from the shore, do so in a way that avoids coming in contact with another surfer. Aiming away from others and towards the “whitewash”, the part of the wave that has been broken already is a safe way to catch a wave. The whitewash has less force and enables one to get past the point break with slightly less effort.  There are other codes of conduct to adhere to, but following these three codes will demonstrate to others in the community that you respect and appreciate the sport.  Aspiring to be a good surfer will help you catch waves, aspiring to be a good person while surfing will help you catch communities.
 
+            ...
 
+            Good luck on your surfing journey! Have fun out there and remember safety over all, especially when looking out for the fresh-water sharks (cue Jaws music)!`,
             coverImage: "image-000.jpg",
             images: [
 <<<<<<< Updated upstream
@@ -41,6 +56,9 @@ The long: look for the jetties. They make it easier and less tiring to paddle ou
                 { filename: "image-22.jpg", caption: "22 image" },
                 { filename: "image-23.jpg", caption: "23 image" }
 =======
+                { filename: "image-001.jpg",},
+                { filename: "image-002.jpg",},
+                { filename: "image-003.jpg",}
 >>>>>>> Stashed changes
             ]
         },
@@ -183,6 +201,16 @@ function _addBlogDetailsToDocument(blog) {
     });
     //console.log(blogText)
 =======
+    let video = document.createElement('video');
+    video.controls = true;
+    let source = document.createElement('source');
+    source.src = "movie.mp4";
+    source.type = "video/mp4";
+    video.appendChild(source);
+    blogDetailContainer.appendChild(video);
+
+    let blogText = document.createElement('p');
+    blogText.innerText = blog.text;
 >>>>>>> Stashed changes
     blogDetailContainer.appendChild(blogText);
 
